dbce776cbea7d5f9a550cc53df3b4bb7036f237d
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-02-26 12:41:05 +0000

    add all tables

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,13 +1,118 @@
 import { mudConfig } from "@latticexyz/world/register";
+import { resolveTableId } from "@latticexyz/config";
 
 export default mudConfig({
   tables: {
-    Tasks: {
+    ObjectTypeMetadata: {
+      keySchema: {
+        objectTypeId: "bytes32",
+      },
       valueSchema: {
-        createdAt: "uint256",
-        completedAt: "uint256",
-        description: "string",
+        isPlayer: "bool",
+        isBlock: "bool",
+        mass: "uint16",
+        stackable: "uint8",
+        durability: "uint16",
+        damage: "uint16",
+        occurence: "bytes4",
+      },
+    },
+    ObjectType: {
+      keySchema: {
+        entityId: "bytes32",
+      },
+      valueSchema: {
+        objectTypeId: "bytes32",
+      },
+    },
+    Position: {
+      keySchema: {
+        entityId: "bytes32",
+      },
+      valueSchema: {
+        x: "int32",
+        y: "int32",
+        z: "int32",
+      },
+    },
+    Player: {
+      keySchema: {
+        player: "address",
+      },
+      valueSchema: {
+        entityId: "bytes32",
+      },
+    },
+    PlayerMetadata: {
+      keySchema: {
+        entityId: "bytes32",
+      },
+      valueSchema: {
+        lastTxBlock: "uint256",
+        numMovesInTx: "uint256"
+      },
+    },
+    Inventory: {
+      keySchema: {
+        entityId: "bytes32",
+      },
+      valueSchema: {
+        ownerEntityId: "bytes32",
+      },
+    },
+    InventoryMetadata: {
+      keySchema: {
+        entityId: "bytes32",
+      },
+      valueSchema: {
+        numObjects: "uint8",
+        numUsesLeft: "uint16",
+      },
+    },
+    Equipped: {
+      keySchema: {
+        entityId: "bytes32",
+      },
+      valueSchema: {
+        inventoryEntityId: "bytes32",
+      },
+    },
+    Health: {
+      keySchema: {
+        entityId: "bytes32",
+      },
+      valueSchema: {
+        lastUpdateBlock: "uint256",
+        health: "uint16",
+      },
+    },
+    Stamina: {
+      keySchema: {
+        entityId: "bytes32",
+      },
+      valueSchema: {
+        lastUpdateBlock: "uint256",
+        stamina: "uint32",
+      },
+    },
+    Recipes: {
+      keySchema: {
+        recipeId: "bytes32",
+      },
+      valueSchema: {
+        stationObjectTypeId: "bytes32",
+        inputObjectTypeIds: "bytes32[]",
+        inputObjectTypeAmounts: "uint8[]",
+        outputObjectTypeIds: "bytes32[]",
+        outputObjectTypeAmounts: "uint8[]",
       },
     },
   },
+  modules: [
+    {
+      name: "UniqueEntityModule",
+      root: true,
+      args: [],
+    },
+  ]
 });
