76f495e4cbd8b4a110af6832b5dcef8ad93e6937
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-04-29 15:21:29 +0100

    write

--- packages/scripts/readWorld.ts
+++ packages/scripts/readWorld.ts
@@ -1,8 +1,27 @@
 import { Hex } from "viem";
 import { setupNetwork } from "./setupNetwork";
+import { resourceToHex } from "@latticexyz/common";
 
 async function main() {
   const { publicClient, worldAddress, IWorldAbi, account, txOptions, callTx } = await setupNetwork();
+
+  const delegation = await publicClient.readContract({
+    address: worldAddress as Hex,
+    abi: IWorldAbi,
+    functionName: "getUserDelegation",
+    args: ["0xE0ae70caBb529336e25FA7a1f036b77ad0089d2a", "0xD50ba6a632Bc7C07e36A8004847007bEf245a69f"],
+    account,
+  });
+  console.log("Delegation:", delegation);
+
+  // const emptyInitCallData = "0x0000000000000000000000000000000000000000000000000000000000000000";
+  // const UNLIMITED_DELEGATION = resourceToHex({ type: "system", namespace: "", name: "unlimited" });
+
+  // await callTx({
+  //   ...txOptions,
+  //   functionName: "registerDelegation",
+  //   args: ["0xe0ae70cabb529336e25fa7a1f036b77ad0089d2a", UNLIMITED_DELEGATION, emptyInitCallData],
+  // });
 
   const floraCoord = { x: 360, y: 16, z: -225 };
   const airCoord = { x: 360, y: 17, z: -225 };
@@ -19,18 +38,18 @@ async function main() {
   });
   console.log("Object Type:", objectTypeAtCoord);
 
-  const simGas = await publicClient.estimateContractGas({
-    ...txOptions,
-    functionName: "computeTerrainObjectTypeIdWithSet",
-    args: [oreCoord],
-  });
-  console.log(simGas.toLocaleString());
+  // const simGas = await publicClient.estimateContractGas({
+  //   ...txOptions,
+  //   functionName: "computeTerrainObjectTypeIdWithSet",
+  //   args: [oreCoord],
+  // });
+  // console.log(simGas.toLocaleString());
 
-  await callTx({
-    ...txOptions,
-    functionName: "computeTerrainObjectTypeIdWithSet",
-    args: [oreCoord],
-  });
+  // await callTx({
+  //   ...txOptions,
+  //   functionName: "computeTerrainObjectTypeIdWithSet",
+  //   args: [oreCoord],
+  // });
 
   process.exit(0);
 }
--- packages/scripts/writeWorld.ts
+++ packages/scripts/writeWorld.ts
@@ -7,7 +7,7 @@ async function main() {
   await callTx({
     ...txOptions,
     functionName: "unregisterDelegation",
-    args: ["0xC1e2405A4CFB42D17e6230c772284E6f99d66908"],
+    args: ["0xD50ba6a632Bc7C07e36A8004847007bEf245a69f"],
   });
 
   process.exit(0);
