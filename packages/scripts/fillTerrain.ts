7c8038a9f135be87246c106f80ab1da117f2300b
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-04-30 22:05:31 +0100

    update progress

--- packages/scripts/fillTerrain.ts
+++ packages/scripts/fillTerrain.ts
@@ -66,7 +66,7 @@ async function main() {
       for (let z = 0; z < rangeZ; z++) {
         txCount += 1;
         console.log("Tx", txCount, "of", numTxs, "(", Math.round((txCount / numTxs) * 100), "% )");
-        if (txCount < 26300) {
+        if (txCount < 28400) {
           console.log("Skipping", txCount);
           continue;
         }
