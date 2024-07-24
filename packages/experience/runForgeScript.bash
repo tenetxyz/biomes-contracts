ebaed6f45e2295ba8cd88933e823e09502a72011
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-07-24 20:04:11 +0100

    upgrade script for experience tables

---
+++ packages/experience/runForgeScript.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
+chainId="31337" # Default to dev mode chain ID
+rpcUrl="http://127.0.0.1:8545"  # Default to dev mode URL
+
+# Loop through all arguments to check for the --prod flag
+if [ "$NODE_ENV" = "testnet" ]; then
+  rpcUrl="https://rpc.garnetchain.com"
+  chainId="17069"
+elif [ "$NODE_ENV" = "mainnet" ]; then
+  rpcUrl="https://rpc.redstonechain.com"
+  chainId="690"
+fi
+
+echo "Using RPC: $rpcUrl"
+echo "Using Chain Id: $chainId"
+
+# Extract worldAddress using awk
+worldAddress=$(awk -v id="$chainId" -F'"' '$2 == id {getline; print $4}' worlds.json)
+
+echo "Using WorldAddress: $worldAddress"
+
+command="forge script $1 --sig 'run(address)' '${worldAddress}' --broadcast --legacy --rpc-url ${rpcUrl}"
+
+# Loop through all arguments to check for the --verbose flag
+for arg in "$@"
+do
+  if [ "$arg" = "--verbose" ]; then
+    echo "Running in verbose mode"
+    command="${command} -vvvv"
+    break  # Exit the loop once the --verbose flag is found
+  fi
+done
+
+echo "Running script: $command"
+
+eval "$command"
---
+++ packages/experience/script/Upgrade.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+
+import { ChipMetadata, ChipMetadataData } from "../src/codegen/tables/ChipMetadata.sol";
+import { IWorld } from "../src/codegen/world/IWorld.sol";
+
+contract PostDeploy is Script {
+  function run(address worldAddress) external {
+    // Specify a store so that you can use tables directly in PostDeploy
+    StoreSwitch.setStoreAddress(worldAddress);
+
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    // Start broadcasting transactions from the deployer account
+    vm.startBroadcast(deployerPrivateKey);
+
+    address chipAddress = 0x712516e61C8B383dF4A63CFe83d7701Bce54B03e;
+    console.log(ChipMetadata.getName(chipAddress));
+    // ChipMetadata.deleteRecord(chipAddress);
+
+    vm.stopBroadcast();
+  }
+}
