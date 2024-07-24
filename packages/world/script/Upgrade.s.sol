6c09071a324998e776599c57d67b9119610370b3
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-07-24 19:56:39 +0100

    update old recipes

--- packages/world/script/Upgrade.s.sol
+++ packages/world/script/Upgrade.s.sol
@@ -8,7 +8,9 @@ import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 import { Unstable_CallWithSignatureSystem } from "@latticexyz/world-modules/src/modules/callwithsignature/Unstable_CallWithSignatureSystem.sol";
 import { DELEGATION_SYSTEM_ID } from "@latticexyz/world-modules/src/modules/callwithsignature/constants.sol";
+import { Recipes } from "../src/codegen/tables/Recipes.sol";
 import { ChestMetadata, ChestMetadataData } from "../src/codegen/tables/ChestMetadata.sol";
+import { BedrockObjectID, ChestObjectID, AnyReinforcedLumberObjectID } from "../src/ObjectTypeIds.sol";
 
 import { IERC165 } from "@latticexyz/store/src/IERC165.sol";
 
@@ -41,28 +43,43 @@ contract Upgrade is Script {
     // Unstable_CallWithSignatureSystem newUpgradedSystem = new Unstable_CallWithSignatureSystem();
     // IWorld(worldAddress).registerSystem(DELEGATION_SYSTEM_ID, newUpgradedSystem, true);
 
-    bytes32[] memory entityIds = new bytes32[](2);
-    entityIds[0] = 0x00000000000000000000000000000000000000000000000000000000000005fe;
-    entityIds[1] = 0x0000000000000000000000000000000000000000000000000000000000000603;
+    // IWorld(worldAddress).initThermoblastObjectTypes();
+    IWorld(worldAddress).initInteractableObjectTypes();
 
-    for (uint i = 0; i < entityIds.length; i++) {
-      bytes32 chestEntityId = entityIds[i];
-      ChestMetadataData memory chestMetadata = ChestMetadata.get(chestEntityId);
-      if (chestMetadata.owner == address(0)) {
-        continue;
-      }
-      console.log("FOUND CHEST");
-      console.logBytes32(chestEntityId);
-      if (chestMetadata.onTransferHook != address(0)) {
-        console.log("REMOVING HOOK");
-        IChestTransferHook(chestMetadata.onTransferHook).onHookRemoved(chestEntityId);
-        ChestMetadata.setOnTransferHook(chestEntityId, address(0));
-      }
-      if (chestMetadata.strength > 0) {
-        console.log("SETTING STRENGTH TO 0");
-        ChestMetadata.setStrength(chestEntityId, 0);
-      }
-    }
+    bytes32 bedrockChestRecipeId = keccak256(
+      abi.encodePacked(BedrockObjectID, uint8(1), ChestObjectID, uint8(1), uint8(163), uint8(1))
+    );
+    bytes32 reinforcedChestRecipeId = keccak256(
+      abi.encodePacked(ChestObjectID, uint8(1), AnyReinforcedLumberObjectID, uint8(1), uint8(162), uint8(1))
+    );
+    console.log("recipe");
+    console.logUint(Recipes.getOutputObjectTypeId(bedrockChestRecipeId));
+    console.logUint(Recipes.getOutputObjectTypeId(reinforcedChestRecipeId));
+    // Recipes.deleteRecord(bedrockChestRecipeId);
+    // Recipes.deleteRecord(reinforcedChestRecipeId);
+
+    // bytes32[] memory entityIds = new bytes32[](2);
+    // entityIds[0] = 0x00000000000000000000000000000000000000000000000000000000000005fe;
+    // entityIds[1] = 0x0000000000000000000000000000000000000000000000000000000000000603;
+
+    // for (uint i = 0; i < entityIds.length; i++) {
+    //   bytes32 chestEntityId = entityIds[i];
+    //   ChestMetadataData memory chestMetadata = ChestMetadata.get(chestEntityId);
+    //   if (chestMetadata.owner == address(0)) {
+    //     continue;
+    //   }
+    //   console.log("FOUND CHEST");
+    //   console.logBytes32(chestEntityId);
+    //   if (chestMetadata.onTransferHook != address(0)) {
+    //     console.log("REMOVING HOOK");
+    //     IChestTransferHook(chestMetadata.onTransferHook).onHookRemoved(chestEntityId);
+    //     ChestMetadata.setOnTransferHook(chestEntityId, address(0));
+    //   }
+    //   if (chestMetadata.strength > 0) {
+    //     console.log("SETTING STRENGTH TO 0");
+    //     ChestMetadata.setStrength(chestEntityId, 0);
+    //   }
+    // }
 
     vm.stopBroadcast();
   }
