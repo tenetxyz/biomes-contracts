b1f44865d9e25f88de7c2bf1b12123abb0f7f745
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-07-24 15:46:31 +0100

    fix deploy experiences missing profile

--- packages/experience/deployToWorld.bash
+++ packages/experience/deployToWorld.bash
@@ -3,13 +3,17 @@
 chainId="31337" # Default to dev mode chain ID
 rpcUrl="http://127.0.0.1:8545"  # Default to dev mode URL
 
+profile = ""
+
 # Loop through all arguments to check for the --prod flag
 if [ "$NODE_ENV" = "testnet" ]; then
   rpcUrl="https://rpc.garnetchain.com"
   chainId="17069"
+  profile="--profile=garnet"
 elif [ "$NODE_ENV" = "mainnet" ]; then
   rpcUrl="https://rpc.redstonechain.com"
   chainId="690"
+  profile="--profile=redstone"
 fi
 
 echo "Using RPC: $rpcUrl"
@@ -20,7 +24,7 @@ worldAddress=$(awk -v id="$chainId" -F'"' '$2 == id {getline; print $4}' ../worl
 
 echo "Using WorldAddress: $worldAddress"
 
-command="pnpm mud deploy --worldAddress=$worldAddress --alwaysRunPostDeploy=true"
+command="pnpm mud deploy --worldAddress=$worldAddress --alwaysRunPostDeploy=true $profile"
 
 echo "Running script: $command"
 
