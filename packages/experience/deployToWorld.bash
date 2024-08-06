c5e2f920ee2bf768a2917832ddff6f6418af08ac
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-08-06 12:47:50 +0100

    add rpc batch param

--- packages/experience/deployToWorld.bash
+++ packages/experience/deployToWorld.bash
@@ -4,16 +4,19 @@ chainId="31337" # Default to dev mode chain ID
 rpcUrl="http://127.0.0.1:8545"  # Default to dev mode URL
 
 profile=""
+rpcBatch="false"
 
 # Loop through all arguments to check for the --prod flag
 if [ "$NODE_ENV" = "testnet" ]; then
   rpcUrl="https://rpc.garnetchain.com"
   chainId="17069"
   profile="--profile=garnet"
+  rpcBatch="true"
 elif [ "$NODE_ENV" = "mainnet" ]; then
   rpcUrl="https://rpc.redstonechain.com"
   chainId="690"
   profile="--profile=redstone"
+  rpcBatch="true"
 fi
 
 echo "Using RPC: $rpcUrl"
@@ -24,7 +27,7 @@ worldAddress=$(awk -v id="$chainId" -F'"' '$2 == id {getline; print $4}' ../worl
 
 echo "Using WorldAddress: $worldAddress"
 
-command="pnpm mud deploy --worldAddress=$worldAddress --alwaysRunPostDeploy=true $profile"
+command="pnpm mud deploy --worldAddress=$worldAddress --alwaysRunPostDeploy=true --rpcBatch=$rpcBatch $profile"
 
 echo "Running script: $command"
 
