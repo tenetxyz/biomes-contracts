44082b9f56c1478edbcd12e58e1cbde8c46de0fe
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-08-28 15:41:40 +0100

    update upgrade script

--- packages/experience/script/Upgrade.s.sol
+++ packages/experience/script/Upgrade.s.sol
@@ -8,6 +8,9 @@ import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 import { ChipMetadata, ChipMetadataData } from "../src/codegen/tables/ChipMetadata.sol";
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 import { Tokens } from "../src/codegen/tables/Tokens.sol";
+import { NFTs } from "../src/codegen/tables/NFTs.sol";
+import { ERC20Metadata } from "../src/codegen/tables/ERC20Metadata.sol";
+import { ERC721Metadata } from "../src/codegen/tables/ERC721Metadata.sol";
 
 contract Upgrade is Script {
   function run(address worldAddress) external {
@@ -20,11 +23,15 @@ contract Upgrade is Script {
     // Start broadcasting transactions from the deployer account
     vm.startBroadcast(deployerPrivateKey);
 
-    // console.logUint(Tokens.lengthTokens(0x1f820052916970Ff09150b58F2f0Fb842C5a58be));
-    // Tokens.deleteRecord(0x1f820052916970Ff09150b58F2f0Fb842C5a58be);
+    // console.logUint(NFTs.lengthNfts(0x3A971f521dde3434B6a3409ABCb77066Dd5123C3));
+    // NFTs.deleteRecord(0x3A971f521dde3434B6a3409ABCb77066Dd5123C3);
+    // console.log(ERC721Metadata.getName(0x45fCbe727564835ACfe428e41997e1c29697bB2B));
+    // ERC721Metadata.deleteRecord(0x45fCbe727564835ACfe428e41997e1c29697bB2B);
+    // console.log(ERC20Metadata.getIcon(0x2FF827f8750dbe1A7dbAD0f7354d0D0395551d2F));
+    // ERC20Metadata.setName(0x2FF827f8750dbe1A7dbAD0f7354d0D0395551d2F, "Settlers Union Bank Coin");
 
-    address chipAddress = 0x7E979136dF1D741991D4e1769B576Ab2D65aC5F3;
-    console.log(ChipMetadata.getName(chipAddress));
+    // address chipAddress = 0x7E979136dF1D741991D4e1769B576Ab2D65aC5F3;
+    // console.log(ChipMetadata.getName(chipAddress));
     // ChipMetadata.deleteRecord(chipAddress);
 
     vm.stopBroadcast();
