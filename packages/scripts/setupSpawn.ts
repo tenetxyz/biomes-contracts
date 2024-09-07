6f116b4c6b6d7017fa9f4a4375af88d8f6f53872
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-09-07 14:16:52 -0600

    disable creating spawn for test

--- packages/scripts/setupSpawn.ts
+++ packages/scripts/setupSpawn.ts
@@ -25,50 +25,50 @@ async function main() {
         functionName: "addSpawn",
         args: [spawnCoord, SPAWN_SIZE],
       },
-      `addSpawn ${spawnCoord.x}, ${spawnCoord.y}, ${spawnCoord.z}`
+      `addSpawn ${spawnCoord.x}, ${spawnCoord.y}, ${spawnCoord.z}`,
     );
 
-    await callTx({
-      ...txOptions,
-      functionName: "initSpawnAreaTop",
-      args: [spawnCoord],
-    });
+    // await callTx({
+    //   ...txOptions,
+    //   functionName: "initSpawnAreaTop",
+    //   args: [spawnCoord],
+    // });
 
-    await callTx({
-      ...txOptions,
-      functionName: "initSpawnAreaTopPart2",
-      args: [spawnCoord],
-    });
+    // await callTx({
+    //   ...txOptions,
+    //   functionName: "initSpawnAreaTopPart2",
+    //   args: [spawnCoord],
+    // });
 
-    await callTx({
-      ...txOptions,
-      functionName: "initSpawnAreaTopAir",
-      args: [spawnCoord],
-    });
+    // await callTx({
+    //   ...txOptions,
+    //   functionName: "initSpawnAreaTopAir",
+    //   args: [spawnCoord],
+    // });
 
-    await callTx({
-      ...txOptions,
-      functionName: "initSpawnAreaTopAirPart2",
-      args: [spawnCoord],
-    });
+    // await callTx({
+    //   ...txOptions,
+    //   functionName: "initSpawnAreaTopAirPart2",
+    //   args: [spawnCoord],
+    // });
 
-    await callTx({
-      ...txOptions,
-      functionName: "initSpawnAreaBottom",
-      args: [spawnCoord],
-    });
+    // await callTx({
+    //   ...txOptions,
+    //   functionName: "initSpawnAreaBottom",
+    //   args: [spawnCoord],
+    // });
 
-    await callTx({
-      ...txOptions,
-      functionName: "initSpawnAreaBottomPart2",
-      args: [spawnCoord],
-    });
+    // await callTx({
+    //   ...txOptions,
+    //   functionName: "initSpawnAreaBottomPart2",
+    //   args: [spawnCoord],
+    // });
 
-    await callTx({
-      ...txOptions,
-      functionName: "initSpawnAreaBottomBorder",
-      args: [spawnCoord],
-    });
+    // await callTx({
+    //   ...txOptions,
+    //   functionName: "initSpawnAreaBottomBorder",
+    //   args: [spawnCoord],
+    // });
   }
 
   console.log("Spawn area setup complete.");
