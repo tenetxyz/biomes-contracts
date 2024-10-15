cf4063ce6687d3d0c657d4ad4c6436c0d9e7d725
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-10-15 12:13:43 +0700

    update xp formula

--- packages/world/src/utils/XPUtils.sol
+++ packages/world/src/utils/XPUtils.sol
@@ -5,16 +5,8 @@ import { ExperiencePoints } from "../codegen/tables/ExperiencePoints.sol";
 
 function mintXP(bytes32 playerEntityId, uint256 initialGas) {
   uint256 gasUsed = initialGas - gasleft();
-  uint256 txFeeGwei = (gasUsed * block.basefee) / 1e9;
-  uint256 xpToMint = txFeeGwei / 750;
+  uint256 txFeeWei = gasUsed * block.basefee;
+  uint256 xpToMint = txFeeWei / 2100000;
   uint256 currentXP = ExperiencePoints._get(playerEntityId);
   ExperiencePoints._set(playerEntityId, currentXP + xpToMint);
-}
-
-function burnXP(bytes32 playerEntityId, uint256 xpToBurn) returns (uint256) {
-  uint256 currentXP = ExperiencePoints._get(playerEntityId);
-  require(currentXP >= xpToBurn, "Player does not have enough xp");
-  uint256 newXP = currentXP - xpToBurn;
-  ExperiencePoints._set(playerEntityId, newXP);
-  return newXP;
 }
