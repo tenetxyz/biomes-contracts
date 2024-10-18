f0fe9b7ab7707ba5bf9451810f9b673cf56fdfec
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-10-18 10:24:34 +0700

    min 1 XP mint

--- packages/world/src/utils/XPUtils.sol
+++ packages/world/src/utils/XPUtils.sol
@@ -7,6 +7,9 @@ function mintXP(bytes32 playerEntityId, uint256 initialGas, uint256 multiplier) 
   uint256 gasUsed = initialGas - gasleft();
   uint256 txFeeWei = gasUsed * block.basefee;
   uint256 xpToMint = txFeeWei / (4200000 * multiplier);
+  if (xpToMint == 0) {
+    xpToMint = 1;
+  }
   uint256 currentXP = ExperiencePoints._get(playerEntityId);
   ExperiencePoints._set(playerEntityId, currentXP + xpToMint);
 }
