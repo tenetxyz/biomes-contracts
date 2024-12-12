4beb10920f73332f4990fd85e50308826e498e6e
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-12-11 21:58:50 -0700

    migration

--- packages/scripts/dozer.ts
+++ packages/scripts/dozer.ts
@@ -12,7 +12,7 @@ async function main() {
   const query = [
     {
       address: worldAddress,
-      query: 'SELECT "toolEntityId", "numUsesLeft" FROM ItemMetadata;',
+      query: 'SELECT "entityId", "chipAddress" FROM Chip;',
     },
   ];
 
@@ -34,11 +34,11 @@ async function main() {
   // console.log(content);
   for (const row of content.result[0]) {
     // don't include the first row cuz its the header
-    if (row[0] == "toolEntityId") continue;
-    // if (row[1].toLowerCase() == "0xD45bE5726Da3347eab4F7Cb151E3bc9De3a18749".toLowerCase()) {
-    //   entityIds.add(row[0]);
-    // }
-    entityIds.add(row[0]);
+    if (row[0] == "entityId") continue;
+    if (row[1].toLowerCase() == "0x4bd5A12B75B24418eCB1285aAAd16a05b94f7096".toLowerCase()) {
+      entityIds.add(row[0]);
+    }
+    // entityIds.add(row[0]);
   }
 
   console.log("entityIds", entityIds);
