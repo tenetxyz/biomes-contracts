2d3675d5a64be4718a1488f366c0197d9cb0d225
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-12-18 18:34:46 -0700

    deploy test erc20 and erc721 on postdeploy to ensure module is installed in world

--- packages/world/script/PostDeploy.s.sol
+++ packages/world/script/PostDeploy.s.sol
@@ -7,6 +7,10 @@ import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 import { VoxelCoord } from "@biomesaw/utils/src/Types.sol";
+import { registerERC20 } from "@latticexyz/world-modules/src/modules/erc20-puppet/registerERC20.sol";
+import { registerERC721 } from "@latticexyz/world-modules/src/modules/erc721-puppet/registerERC721.sol";
+import { ERC721MetadataData as MUDERC721MetadataData } from "@latticexyz/world-modules/src/modules/erc721-puppet/tables/ERC721Metadata.sol";
+import { ERC20MetadataData as MUDERC20MetadataData } from "@latticexyz/world-modules/src/modules/erc20-puppet/tables/ERC20Metadata.sol";
 
 contract PostDeploy is Script {
   function run(address worldAddress) external {
@@ -18,6 +22,22 @@ contract PostDeploy is Script {
 
     // Start broadcasting transactions from the deployer account
     vm.startBroadcast(deployerPrivateKey);
+
+    // Register a sample ERC721, so that the ERC721 Puppet Module is installed
+    // TODO: Figure out a way to do this without having to manually register a ERC721
+    registerERC721(
+      IWorld(worldAddress),
+      bytes14("test-721"),
+      MUDERC721MetadataData({ symbol: "test-721", name: "test-721", baseURI: "" })
+    );
+
+    // Register a sample ERC20, so that the ERC20 Puppet Module is installed
+    // TODO: Figure out a way to do this without having to manually register a ERC20
+    registerERC20(
+      IWorld(worldAddress),
+      bytes14("test-20"),
+      MUDERC20MetadataData({ symbol: "test-20", name: "test-20", decimals: 18 })
+    );
 
     IWorld(worldAddress).initPlayerObjectTypes();
     IWorld(worldAddress).initTerrainBlockObjectTypes();
