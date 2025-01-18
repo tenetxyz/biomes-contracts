0aa3fafd64a33e37e2501b9397ab597fac152b84
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-01-18 15:24:06 -0700

    wip

--- packages/world/src/systems/PipeTransferSystem.sol
+++ packages/world/src/systems/PipeTransferSystem.sol
@@ -19,6 +19,13 @@ import { ChipOnPipeTransferData, TransferData } from "../Types.sol";
 import { isStorageContainer } from "../utils/ObjectTypeUtils.sol";
 import { pipeTransferCommon, PipeTransferCommonContext } from "../utils/TransferUtils.sol";
 
+struct PipeMultiTransferData {
+  bytes32 targetEntityId;
+  VoxelCoordDirectionVonNeumann[] path;
+  TransferData transferData;
+  bytes extraData;
+}
+
 contract PipeTransferSystem is System {
   function requireAllowed(
     ChipData memory checkChipData,
@@ -54,37 +61,131 @@ contract PipeTransferSystem is System {
   }
 
   function pipeTransfer(
-    bytes32 srcEntityId,
-    bytes32 dstEntityId,
-    VoxelCoordDirectionVonNeumann[] memory path,
-    uint8 transferObjectTypeId,
-    uint16 numToTransfer,
-    bytes memory extraData
+    bytes32 callerEntityId,
+    bool isDeposit,
+    PipeMultiTransferData[] memory pipesTransferData
   ) public payable {
-    PipeTransferCommonContext memory ctx = pipeTransferCommon(srcEntityId, dstEntityId, path);
+    require(!IN_MAINTENANCE, "Biomes is in maintenance mode. Try again later");
+    uint8 callerObjectTypeId = ObjectType._get(callerEntityId);
+    require(isStorageContainer(callerObjectTypeId), "PipeTransferSystem: source object type is not a chest");
 
-    require(!ObjectTypeMetadata._getIsTool(transferObjectTypeId), "PipeTransferSystem: object type is not a block");
-    require(numToTransfer > 0, "PipeTransferSystem: amount must be greater than 0");
-    removeFromInventoryCount(ctx.baseSrcEntityId, transferObjectTypeId, numToTransfer);
+    uint8 transferObjectTypeId = NullObjectTypeId;
+    uint256 totalNumToTransfer = 0;
 
-    if (isStorageContainer(ctx.dstObjectTypeId)) {
-      addToInventoryCount(ctx.baseDstEntityId, ctx.dstObjectTypeId, transferObjectTypeId, numToTransfer);
-    } else if (ctx.dstObjectTypeId == ForceFieldObjectID) {
-      require(
-        transferObjectTypeId == ChipBatteryObjectID,
-        "PipeTransferSystem: force field can only accept chip batteries"
-      );
-      uint256 newBatteryLevel = ctx.checkChipData.batteryLevel + (uint256(numToTransfer) * CHARGE_PER_BATTERY);
+    VoxelCoord memory callerCoord = positionDataToVoxelCoord(Position._get(callerEntityId));
+    ChipData memory callerChipData = updateChipBatteryLevel(callerEntityId);
+    bytes32 callerForceFieldEntityId = getForceField(callerCoord);
+    if (callerForceFieldEntityId != bytes32(0)) {
+      ChipData memory callerForceFieldChipData = updateChipBatteryLevel(callerForceFieldEntityId);
+      callerChipData.batteryLevel += callerForceFieldChipData.batteryLevel;
+    }
+    require(callerChipData.chipAddress == _msgSender(), "PipeTransferSystem: caller is not the chip of the smart item");
+    require(callerChipData.batteryLevel > 0, "PipeTransferSystem: caller has no charge");
 
-      Chip._setBatteryLevel(ctx.baseDstEntityId, newBatteryLevel);
-      Chip._setLastUpdatedTime(ctx.baseDstEntityId, block.timestamp);
+    for (uint i = 0; i < pipesTransferData.length; i++) {
+      PipeMultiTransferData memory pipeTransferData = pipesTransferData[i];
+      if (transferObjectTypeId == NullObjectTypeId) {
+        transferObjectTypeId = pipeTransferData.transferData.objectTypeId;
+      } else {
+        require(
+          transferObjectTypeId == pipeTransferData.transferData.objectTypeId,
+          "PipeTransferSystem: all transfers must be of the same object type"
+        );
+      }
+      totalNumToTransfer += pipeTransferData.transferData.numToTransfer;
 
-      safeCallChip(
-        ctx.checkChipData.chipAddress,
-        abi.encodeCall(IChip.onPowered, (ctx.baseSrcEntityId, ctx.baseDstEntityId, numToTransfer))
-      );
-    } else {
-      revert("PipeTransferSystem: destination object type is not valid");
+      require(pipeTransferData.targetEntityId != callerEntityId, "PipeTransferSystem: cannot transfer to self");
+      VoxelCoord memory targetCoord = positionDataToVoxelCoord(Position._get(pipeTransferData.targetEntityId));
+      ChipData memory targetChipData = updateChipBatteryLevel(pipeTransferData.targetEntityId);
+      uint8 targetObjectTypeId = ObjectType._get(pipeTransferData.targetEntityId);
+      if (targetObjectTypeId != ForceFieldObjectID) {
+        bytes32 targetForceFieldEntityId = getForceField(targetCoord);
+        if (targetForceFieldEntityId != bytes32(0)) {
+          ChipData memory targetForceFieldChipData = updateChipBatteryLevel(targetForceFieldEntityId);
+          targetChipData.batteryLevel += targetForceFieldChipData.batteryLevel;
+        }
+      }
+
+      requireValidPath(callerCoord, targetCoord, pipeTransferData.path);
+
+      if (pipeTransferData.transferData.toolEntityIds.length == 0) {
+        require(
+          !ObjectTypeMetadata._getIsTool(pipeTransferData.transferData.objectTypeId),
+          "PipeTransferSystem: object type is not a block"
+        );
+        require(pipeTransferData.transferData.numToTransfer > 0, "PipeTransferSystem: amount must be greater than 0");
+
+        if (isDeposit) {
+          removeFromInventoryCount(
+            callerEntityId,
+            pipeTransferData.transferData.objectTypeId,
+            pipeTransferData.transferData.numToTransfer
+          );
+
+          if (isStorageContainer(targetObjectTypeId)) {
+            addToInventoryCount(
+              targetEntityId,
+              targetObjectTypeId,
+              pipeTransferData.transferData.objectTypeId,
+              pipeTransferData.transferData.numToTransfer
+            );
+          } else if (targetObjectTypeId == ForceFieldObjectID) {
+            require(
+              pipeTransferData.transferData.objectTypeId == ChipBatteryObjectID,
+              "PipeTransferSystem: force field can only accept chip batteries"
+            );
+            uint256 newBatteryLevel = targetChipData.batteryLevel +
+              (uint256(pipeTransferData.transferData.numToTransfer) * CHARGE_PER_BATTERY);
+
+            Chip._setBatteryLevel(targetEntityId, newBatteryLevel);
+            Chip._setLastUpdatedTime(targetEntityId, block.timestamp);
+
+            safeCallChip(
+              targetChipData.chipAddress,
+              abi.encodeCall(
+                IChip.onPowered,
+                (callerEntityId, targetEntityId, pipeTransferData.transferData.numToTransfer)
+              )
+            );
+          } else {
+            revert("PipeTransferSystem: target object type is not valid");
+          }
+        } else {
+          addToInventoryCount(
+            callerEntityId,
+            callerObjectTypeId,
+            pipeTransferData.transferData.objectTypeId,
+            pipeTransferData.transferData.numToTransfer
+          );
+
+          if (isStorageContainer(targetObjectTypeId)) {
+            removeFromInventoryCount(
+              pipeTransferData.targetEntityId,
+              pipeTransferData.transferData.objectTypeId,
+              pipeTransferData.transferData.numToTransfer
+            );
+          } else {
+            revert("PipeTransferSystem: target object type is not valid");
+          }
+        }
+      } else {
+        require(
+          pipeTransferData.transferData.toolEntityIds.length < type(uint16).max,
+          "PipeTransferSystem: too many tools to transfer"
+        );
+
+        for (uint i = 0; i < pipeTransferData.transferData.toolEntityIds.length; i++) {
+          uint8 toolObjectTypeId = transferInventoryTool(
+            isDeposit ? callerEntityId : pipeTransferData.targetEntityId,
+            isDeposit ? pipeTransferData.targetEntityId : callerEntityId,
+            pipeTransferData.transferData.objectTypeId,
+            pipeTransferData.transferData.toolEntityIds[i]
+          );
+          require(toolObjectTypeId == transferObjectTypeId, "PipeTransferSystem: all tools must be of the same type");
+        }
+      }
+
+      // TODO: requireAllowed on target
     }
 
     // Note: we call this after the transfer state has been updated, to prevent re-entrancy attacks
@@ -101,87 +202,5 @@ contract PipeTransferSystem is System {
         extraData
       );
     }
-  }
-
-  function pipeTransferTool(
-    bytes32 srcEntityId,
-    bytes32 dstEntityId,
-    VoxelCoordDirectionVonNeumann[] memory path,
-    bytes32 toolEntityId,
-    bytes memory extraData
-  ) public payable {
-    bytes32[] memory toolEntityIds = new bytes32[](1);
-    toolEntityIds[0] = toolEntityId;
-    pipeTransferTools(srcEntityId, dstEntityId, path, toolEntityIds, extraData);
-  }
-
-  function pipeTransferTools(
-    bytes32 srcEntityId,
-    bytes32 dstEntityId,
-    VoxelCoordDirectionVonNeumann[] memory path,
-    bytes32[] memory toolEntityIds,
-    bytes memory extraData
-  ) public payable {
-    require(toolEntityIds.length > 0, "PipeTransferSystem: must transfer at least one tool");
-    require(toolEntityIds.length < type(uint16).max, "PipeTransferSystem: too many tools to transfer");
-
-    PipeTransferCommonContext memory ctx = pipeTransferCommon(srcEntityId, dstEntityId, path);
-    require(isStorageContainer(ctx.dstObjectTypeId), "PipeTransferSystem: destination object type is not valid");
-
-    uint8 toolObjectTypeId;
-    for (uint i = 0; i < toolEntityIds.length; i++) {
-      uint8 currentToolObjectTypeId = transferInventoryTool(
-        ctx.baseSrcEntityId,
-        ctx.baseDstEntityId,
-        ctx.dstObjectTypeId,
-        toolEntityIds[i]
-      );
-      if (i > 0) {
-        require(toolObjectTypeId == currentToolObjectTypeId, "PipeTransferSystem: all tools must be of the same type");
-      } else {
-        toolObjectTypeId = currentToolObjectTypeId;
-      }
-    }
-
-    // Note: we call this after the transfer state has been updated, to prevent re-entrancy attacks
-    requireAllowed(
-      ctx.checkChipData,
-      ctx.isDeposit,
-      ctx.baseSrcEntityId,
-      ctx.baseDstEntityId,
-      path,
-      toolObjectTypeId,
-      uint16(toolEntityIds.length),
-      toolEntityIds,
-      extraData
-    );
-  }
-
-  function pipeTransfer(
-    bytes32 srcEntityId,
-    bytes32 dstEntityId,
-    VoxelCoordDirectionVonNeumann[] memory path,
-    uint8 transferObjectTypeId,
-    uint16 numToTransfer
-  ) public payable {
-    pipeTransfer(srcEntityId, dstEntityId, path, transferObjectTypeId, numToTransfer, new bytes(0));
-  }
-
-  function pipeTransferTool(
-    bytes32 srcEntityId,
-    bytes32 dstEntityId,
-    VoxelCoordDirectionVonNeumann[] memory path,
-    bytes32 toolEntityId
-  ) public payable {
-    pipeTransferTool(srcEntityId, dstEntityId, path, toolEntityId, new bytes(0));
-  }
-
-  function pipeTransferTools(
-    bytes32 srcEntityId,
-    bytes32 dstEntityId,
-    VoxelCoordDirectionVonNeumann[] memory path,
-    bytes32[] memory toolEntityIds
-  ) public payable {
-    pipeTransferTools(srcEntityId, dstEntityId, path, toolEntityIds, new bytes(0));
   }
 }
