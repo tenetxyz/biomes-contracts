cc89afa2747917fd1b64c79a9e125adc09c1a22e
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-01-25 11:09:59 -0700

    output stats

--- packages/scripts/parseStats.ts
+++ packages/scripts/parseStats.ts
@@ -4,7 +4,9 @@ import path from "path";
 import { formatEther, parseAbi } from "viem";
 import { setupNetwork } from "./setupNetwork";
 import os from "os";
-import { replacer } from "./utils";
+import { replacer, reviver } from "./utils";
+
+const wordlDeployer = "0x1f820052916970Ff09150b58F2f0Fb842C5a58be";
 
 async function main() {
   const startTime = Date.now();
@@ -12,6 +14,9 @@ async function main() {
   // Setup initial configuration
   const { publicClient, worldAddress, allAbis, account, txOptions, callTx, fromBlock, indexerUrl } =
     await setupNetwork();
+
+  const userNamesData = await fetch("https://biome1.biomes.aw/api/user/names").then((res) => res.json());
+  const addressToName = userNamesData.addressToName;
 
   const query = [
     {
@@ -21,8 +26,6 @@ async function main() {
   ];
 
   console.log("indexerUrl", indexerUrl);
-  console.log("query", query);
-
   const delegations = new Map<string, string>();
 
   // fetch post request
@@ -40,54 +43,86 @@ async function main() {
     delegations.set(row[1].toLowerCase(), row[0].toLowerCase());
   }
 
-  const stats = JSON.parse(fs.readFileSync("gen/stats.json", "utf8"));
+  const stats = JSON.parse(fs.readFileSync("gen/stats.json", "utf8"), reviver);
   const allUserStats = stats.allUserStats;
-
-  const SUB_NFT_ADDRESS = "0xE0aC150d02e4a9808403F94a289bcEc20d30A3fB";
 
   const startDate = new Date("2024-10-01T00:00:00.000Z");
 
-  const users: Set<string> = new Set();
+  const functionNames: Set<string> = new Set();
+
   for (const user in allUserStats) {
-    if (allUserStats[user].numTxs < 50) continue;
+    // if (allUserStats[user].numTxs < 50) continue;
+    const txCounts = allUserStats[user].txCounts;
+    // console.log("txCounts", txCounts);
+    for (const fnName of txCounts.keys()) {
+      functionNames.add(fnName);
+    }
 
     const firstTxDate = new Date(allUserStats[user].firstTxDate);
-    if (firstTxDate >= startDate) {
-      const delegator = delegations.get(user);
-      if (delegator) {
-        users.add(delegator);
-      } else {
-        users.add(user);
-      }
-    }
   }
-  console.log("users", users);
-  console.log("numUsers", users.size);
 
-  let numUsersWithSubNFT = 0;
-  let processedUsers = 0;
-  // Process users in batches of 100
-  const batchSize = 100;
-  const userArray = Array.from(users);
+  const csvRows = [
+    [
+      "Address",
+      "Username",
+      "First Tx Date",
+      "Last Tx Date",
+      "Total # Txs",
+      "Total Fees (wei)",
+      "Total Fees (Eth)",
+      "Avg Total Fees (wei)",
+      "Avg Total Fees (Eth)",
+      "Total L1 Fees (wei)",
+      "Total L1 Fees (Eth)",
+      "Total L2 Fees (wei)",
+      "Total L2 Fees (Eth)",
+      "Total Base Fees (wei)",
+      "Total Base Fees (Eth)",
+      "Total Priority Fees (wei)",
+      "Total Priority Fees (Eth)",
+    ],
+  ];
+  const fnNameArr = ["Address", "Username", ...[...functionNames]];
+  const fnNameCsvRows = [fnNameArr.map((fn) => `# ${fn}`)];
+  console.log("Processing users...");
+  for (const user in allUserStats) {
+    if (user.toLowerCase() === wordlDeployer.toLowerCase()) continue;
 
-  for (let i = 0; i < userArray.length; i += batchSize) {
-    const batch = userArray.slice(i, i + batchSize);
-    const balances = await Promise.all(
-      batch.map((user) =>
-        publicClient.readContract({
-          address: SUB_NFT_ADDRESS,
-          abi: parseAbi(["function balanceOf(address account) external view returns (uint256)"]),
-          functionName: "balanceOf",
-          args: [user as `0x${string}`],
-        }),
-      ),
-    );
-
-    numUsersWithSubNFT += balances.filter((balance) => balance > 0).length;
-    processedUsers += batch.length;
-    console.log(`Processed ${processedUsers} users`);
+    const txCounts = allUserStats[user].txCounts;
+    const firstTxDate = new Date(allUserStats[user].firstTxDate);
+    const lastTxDate = new Date(allUserStats[user].lastTxDate);
+    const delegator = delegations.get(user);
+    const avgTotalFeesWei =
+      allUserStats[user].numTxs > 0 ? allUserStats[user].totalFees / BigInt(allUserStats[user].numTxs) : 0n;
+    // console.log(txCounts);
+    const username = addressToName[user];
+    csvRows.push([
+      user,
+      username,
+      firstTxDate.toLocaleDateString(),
+      lastTxDate.toLocaleDateString(),
+      allUserStats[user].numTxs,
+      allUserStats[user].totalFees,
+      formatEther(allUserStats[user].totalFees),
+      avgTotalFeesWei,
+      formatEther(avgTotalFeesWei),
+      allUserStats[user].totalL1Fees,
+      formatEther(allUserStats[user].totalL1Fees),
+      allUserStats[user].totalL2Fees,
+      formatEther(allUserStats[user].totalL2Fees),
+      allUserStats[user].totalBaseFees,
+      formatEther(allUserStats[user].totalBaseFees),
+      allUserStats[user].totalPriorityFees,
+      formatEther(allUserStats[user].totalPriorityFees),
+    ]);
+    fnNameCsvRows.push([user, username, ...fnNameArr.map((fn) => txCounts.get(fn) ?? 0)]);
   }
-  console.log("numUsersWithSubNFT", numUsersWithSubNFT);
+  console.log("csvRows", csvRows.length);
+  console.log("fnNameCsvRows", fnNameCsvRows.length);
+
+  // write to file
+  fs.writeFileSync("gen/userStats.csv", csvRows.map((row) => row.join(",")).join("\n"));
+  fs.writeFileSync("gen/userStatsFn.csv", fnNameCsvRows.map((row) => row.join(",")).join("\n"));
 
   console.log("Finished!");
 }
