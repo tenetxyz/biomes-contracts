1c7b33edb87acc2a00afe6a2ab85c12e4fa8445f
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-02-03 12:59:53 -0700

    set oracle address

--- packages/world/src/systems/admin/OracleSystem.sol
+++ packages/world/src/systems/admin/OracleSystem.sol
@@ -8,15 +8,16 @@ import { ROOT_NAMESPACE_ID } from "@latticexyz/world/src/constants.sol";
 
 import { BlockHash } from "../../codegen/tables/BlockHash.sol";
 import { BlockPrevrandao } from "../../codegen/tables/BlockPrevrandao.sol";
+import { ORACLE_ADDRESS } from "../../Constants.sol";
 
 contract OracleSystem is System {
   function setBlockHash(uint256 blockNumber, bytes32 blockHash) public {
-    AccessControl.requireOwner(ROOT_NAMESPACE_ID, _msgSender());
+    require(_msgSender() == ORACLE_ADDRESS, "Only the oracle can set the block hash");
     BlockHash._set(blockNumber, blockHash);
   }
 
   function setBlockPrevrandao(uint256 blockNumber, uint256 blockPrevrandao) public {
-    AccessControl.requireOwner(ROOT_NAMESPACE_ID, _msgSender());
+    require(_msgSender() == ORACLE_ADDRESS, "Only the oracle can set the block prevrandao");
     BlockPrevrandao._set(blockNumber, blockPrevrandao);
   }
 }
