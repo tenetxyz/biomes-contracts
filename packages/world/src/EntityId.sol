1bf8e9f4d3a1b11bb05f877556bd80c2b9dfe607
vdrg <vdrg@protonmail.ch>
2025-02-11 19:16:26 -0300

    Use get instead of _get in baseEntityId()

--- packages/world/src/EntityId.sol
+++ packages/world/src/EntityId.sol
@@ -6,7 +6,7 @@ import { BaseEntity } from "./codegen/tables/BaseEntity.sol";
 type EntityId is bytes32;
 
 function baseEntityId(EntityId self) view returns (EntityId) {
-  EntityId base = BaseEntity._get(self);
+  EntityId base = BaseEntity.get(self);
   return EntityId.unwrap(base) == bytes32(0) ? self : base;
 }
 
