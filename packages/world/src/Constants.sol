b23f797cd2a34ded2ebfe6ba6952a15d822d4f59
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-02-17 13:44:57 -0700

    spawn and drain

--- packages/world/src/Constants.sol
+++ packages/world/src/Constants.sol
@@ -24,15 +24,34 @@ uint256 constant MIN_TIME_BEFORE_AUTO_LOGOFF = 15 minutes;
 int32 constant MAX_PLAYER_INFLUENCE_HALF_WIDTH = 10;
 int32 constant MAX_PLAYER_RESPAWN_HALF_WIDTH = 10;
 
-address constant ORACLE_ADDRESS = 0xe67AcfFCe0B542540F1520a9eaD7Aa86ff31196E;
-
-uint128 constant MAX_PLAYER_ENERGY = 100_000;
-uint128 constant MIN_PLAYER_ENERGY = MAX_PLAYER_ENERGY / 10;
-
-uint128 constant SPAWN_ENERGY = 100;
 int32 constant SPAWN_AREA_HALF_WIDTH = 5;
 
 int32 constant CHUNK_SIZE = 16;
 int32 constant AREA_SIZE = 512;
 
 uint256 constant SAFE_CHIP_GAS = 1_000_000;
+
+address constant ORACLE_ADDRESS = 0xe67AcfFCe0B542540F1520a9eaD7Aa86ff31196E;
+
+// ------------------------------------------------------------
+// Values To Tune
+// ------------------------------------------------------------
+uint128 constant MAX_PLAYER_ENERGY = 100_000;
+uint128 constant PLAYER_ENERGY_DRAIN_RATE = 100;
+uint128 constant PLAYER_ENERGY_DRAIN_INTERVAL = 1 seconds;
+uint128 constant MIN_PLAYER_ENERGY_THRESHOLD_TO_DRAIN = MAX_PLAYER_ENERGY / 10;
+
+uint128 constant MACHINE_ENERGY_DRAIN_RATE = 100;
+uint128 constant MACHINE_ENERGY_DRAIN_INTERVAL = 1 seconds;
+uint128 constant MIN_MACHINE_ENERGY_THRESHOLD_TO_DRAIN = 0;
+
+uint128 constant SMART_CHEST_ENERGY_COST = 100;
+
+uint128 constant PLAYER_BUILD_ENERGY_COST = 100;
+uint128 constant PLAYER_MINE_ENERGY_COST = 100;
+uint128 constant PLAYER_HIT_ENERGY_COST = 100;
+uint128 constant PLAYER_MOVE_ENERGY_COST = 100;
+uint128 constant PLAYER_CRAFT_ENERGY_COST = 100;
+uint128 constant PLAYER_DROP_ENERGY_COST = 100;
+uint128 constant PLAYER_PICKUP_ENERGY_COST = 100;
+uint128 constant PLAYER_TRANSFER_ENERGY_COST = 100;
--- packages/world/src/utils/EnergyUtils.sol
+++ packages/world/src/utils/EnergyUtils.sol
@@ -4,23 +4,36 @@ pragma solidity >=0.8.24;
 import { Energy, EnergyData } from "../codegen/tables/Energy.sol";
 
 import { EntityId } from "../EntityId.sol";
-import { MIN_PLAYER_ENERGY } from "../Constants.sol";
+import { PLAYER_ENERGY_DRAIN_RATE, PLAYER_ENERGY_DRAIN_INTERVAL, MIN_PLAYER_ENERGY_THRESHOLD_TO_DRAIN } from "../Constants.sol";
+import { MACHINE_ENERGY_DRAIN_RATE, MACHINE_ENERGY_DRAIN_INTERVAL, MIN_MACHINE_ENERGY_THRESHOLD_TO_DRAIN } from "../Constants.sol";
 
-function massToEnergy(uint32 mass) pure returns (uint128) {
+function massToEnergy(uint128 mass) pure returns (uint128) {
   return uint128(mass * 50);
 }
 
-function getLatestEnergyData(EntityId entityId, uint128 minEnergy) view returns (EnergyData memory) {
+function energyToMass(uint128 energy) pure returns (uint128) {
+  return uint128(energy / 50);
+}
+
+function getLatestEnergyData(
+  EntityId entityId,
+  uint128 drainEnergyThreshold,
+  uint128 drainInterval,
+  uint128 drainRate
+) view returns (EnergyData memory) {
   EnergyData memory energyData = Energy._get(entityId);
 
-  if (energyData.energy > minEnergy) {
+  if (energyData.energy > drainEnergyThreshold) {
     // Calculate how much time has passed since last update
     uint128 timeSinceLastUpdate = uint128(block.timestamp) - energyData.lastUpdatedTime;
     if (timeSinceLastUpdate == 0) {
       return energyData;
     }
-
-    uint128 newEnergy = energyData.energy > timeSinceLastUpdate ? energyData.energy - timeSinceLastUpdate : 0;
+    if (timeSinceLastUpdate < drainInterval) {
+      return energyData;
+    }
+    uint128 energyDrained = (timeSinceLastUpdate * drainRate) / drainInterval;
+    uint128 newEnergy = energyData.energy > energyDrained ? energyData.energy - energyDrained : 0;
     energyData.energy = newEnergy;
     energyData.lastUpdatedTime = uint128(block.timestamp);
   }
@@ -28,24 +41,34 @@ function getLatestEnergyData(EntityId entityId, uint128 minEnergy) view returns 
   return energyData;
 }
 
-function updateEnergyLevel(EntityId entityId, uint128 minEnergy) returns (EnergyData memory) {
-  EnergyData memory energyData = getLatestEnergyData(entityId, minEnergy);
+function getPlayerEnergyLevel(EntityId entityId) view returns (EnergyData memory) {
+  return
+    getLatestEnergyData(
+      entityId,
+      MIN_PLAYER_ENERGY_THRESHOLD_TO_DRAIN,
+      PLAYER_ENERGY_DRAIN_INTERVAL,
+      PLAYER_ENERGY_DRAIN_RATE
+    );
+}
+
+function updatePlayerEnergyLevel(EntityId entityId) returns (EnergyData memory) {
+  EnergyData memory energyData = getPlayerEnergyLevel(entityId);
   Energy._set(entityId, energyData);
   return energyData;
 }
 
-function getPlayerEnergyLevel(EntityId entityId) view returns (EnergyData memory) {
-  return getLatestEnergyData(entityId, MIN_PLAYER_ENERGY);
-}
-
-function updatePlayerEnergyLevel(EntityId entityId) returns (EnergyData memory) {
-  return updateEnergyLevel(entityId, MIN_PLAYER_ENERGY);
-}
-
 function getMachineEnergyLevel(EntityId entityId) view returns (EnergyData memory) {
-  return getLatestEnergyData(entityId, 0);
+  return
+    getLatestEnergyData(
+      entityId,
+      MIN_MACHINE_ENERGY_THRESHOLD_TO_DRAIN,
+      MACHINE_ENERGY_DRAIN_INTERVAL,
+      MACHINE_ENERGY_DRAIN_RATE
+    );
 }
 
 function updateMachineEnergyLevel(EntityId entityId) returns (EnergyData memory) {
-  return updateEnergyLevel(entityId, 0);
+  EnergyData memory energyData = getMachineEnergyLevel(entityId);
+  Energy._set(entityId, energyData);
+  return energyData;
 }
