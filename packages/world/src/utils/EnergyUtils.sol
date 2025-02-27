0d01dff8bb90adca060780f273dfefade895ab3c
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-02-27 12:30:37 -0700

    add drained energy to local pool

--- packages/world/src/utils/EnergyUtils.sol
+++ packages/world/src/utils/EnergyUtils.sol
@@ -3,12 +3,15 @@ pragma solidity >=0.8.24;
 
 import { Energy, EnergyData } from "../codegen/tables/Energy.sol";
 import { LocalEnergyPool } from "../codegen/tables/LocalEnergyPool.sol";
+import { Position, PositionData } from "../codegen/tables/Position.sol";
 
-import { VoxelCoord } from "../VoxelCoord.sol";
+import { VoxelCoord, VoxelCoordLib } from "../VoxelCoord.sol";
 import { EntityId } from "../EntityId.sol";
 import { MASS_TO_ENERGY_MULTIPLIER } from "../Constants.sol";
-import { PLAYER_ENERGY_DRAIN_RATE, PLAYER_ENERGY_DRAIN_INTERVAL, MIN_PLAYER_ENERGY_THRESHOLD_TO_DRAIN } from "../Constants.sol";
-import { MACHINE_ENERGY_DRAIN_RATE, MACHINE_ENERGY_DRAIN_INTERVAL, MIN_MACHINE_ENERGY_THRESHOLD_TO_DRAIN } from "../Constants.sol";
+import { PLAYER_ENERGY_DRAIN_RATE, PLAYER_ENERGY_DRAIN_INTERVAL } from "../Constants.sol";
+import { MACHINE_ENERGY_DRAIN_RATE, MACHINE_ENERGY_DRAIN_INTERVAL } from "../Constants.sol";
+
+using VoxelCoordLib for PositionData;
 
 function massToEnergy(uint128 mass) pure returns (uint128) {
   return uint128(mass * MASS_TO_ENERGY_MULTIPLIER);
@@ -20,58 +23,50 @@ function energyToMass(uint128 energy) pure returns (uint128) {
 
 function getLatestEnergyData(
   EntityId entityId,
-  uint128 drainEnergyThreshold,
   uint128 drainInterval,
   uint128 drainRate
-) view returns (EnergyData memory) {
+) view returns (EnergyData memory, uint128) {
   EnergyData memory energyData = Energy._get(entityId);
+  uint128 energyDrained = 0;
 
-  if (energyData.energy > drainEnergyThreshold) {
+  if (energyData.energy > 0) {
     // Calculate how much time has passed since last update
     uint128 timeSinceLastUpdate = uint128(block.timestamp) - energyData.lastUpdatedTime;
     if (timeSinceLastUpdate == 0) {
-      return energyData;
+      return (energyData, energyDrained);
     }
     if (timeSinceLastUpdate < drainInterval) {
-      return energyData;
+      return (energyData, energyDrained);
     }
-    uint128 energyDrained = (timeSinceLastUpdate * drainRate) / drainInterval;
-    uint128 newEnergy = energyData.energy > energyDrained ? energyData.energy - energyDrained : 0;
-    energyData.energy = newEnergy;
+    energyDrained = (timeSinceLastUpdate * drainRate) / drainInterval;
+    energyDrained = energyDrained > energyData.energy ? energyData.energy : energyDrained;
+    energyData.energy = energyData.energy - energyDrained;
     energyData.lastUpdatedTime = uint128(block.timestamp);
   }
 
-  return energyData;
+  return (energyData, energyDrained);
 }
 
-function getPlayerEnergyLevel(EntityId entityId) view returns (EnergyData memory) {
-  return
-    getLatestEnergyData(
-      entityId,
-      MIN_PLAYER_ENERGY_THRESHOLD_TO_DRAIN,
-      PLAYER_ENERGY_DRAIN_INTERVAL,
-      PLAYER_ENERGY_DRAIN_RATE
-    );
+function getPlayerEnergyLevel(EntityId entityId) view returns (EnergyData memory, uint128) {
+  return getLatestEnergyData(entityId, PLAYER_ENERGY_DRAIN_INTERVAL, PLAYER_ENERGY_DRAIN_RATE);
 }
 
-function updatePlayerEnergyLevel(EntityId entityId) returns (EnergyData memory) {
-  EnergyData memory energyData = getPlayerEnergyLevel(entityId);
+function updatePlayerEnergyLevel(EntityId entityId) returns (EnergyData memory, uint128) {
+  (EnergyData memory energyData, uint128 energyDrained) = getPlayerEnergyLevel(entityId);
   Energy._set(entityId, energyData);
-  return energyData;
+  return (energyData, energyDrained);
 }
 
-function getMachineEnergyLevel(EntityId entityId) view returns (EnergyData memory) {
-  return
-    getLatestEnergyData(
-      entityId,
-      MIN_MACHINE_ENERGY_THRESHOLD_TO_DRAIN,
-      MACHINE_ENERGY_DRAIN_INTERVAL,
-      MACHINE_ENERGY_DRAIN_RATE
-    );
+function getMachineEnergyLevel(EntityId entityId) view returns (EnergyData memory, uint128) {
+  return getLatestEnergyData(entityId, MACHINE_ENERGY_DRAIN_INTERVAL, MACHINE_ENERGY_DRAIN_RATE);
 }
 
 function updateMachineEnergyLevel(EntityId entityId) returns (EnergyData memory) {
-  EnergyData memory energyData = getMachineEnergyLevel(entityId);
+  (EnergyData memory energyData, uint128 energyDrained) = getMachineEnergyLevel(entityId);
+  if (energyDrained > 0) {
+    VoxelCoord memory machineCoord = Position._get(entityId).toVoxelCoord();
+    machineCoord.addEnergyToLocalPool(energyDrained);
+  }
   Energy._set(entityId, energyData);
   return energyData;
 }
